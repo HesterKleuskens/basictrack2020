adf672199117c8da0e8791f19c67ea06b8653648
HesterKleuskens <hesterkleuskens23@gmail.com>
2020-09-22 12:54:19 +0200

    Demo about variables

--- week_2/variables.py
+++ week_2/variables.py
@@ -1,6 +1,8 @@
-"#get information"
-ask_user=float(input("please give me a number"))
-"#do calculation"
-calculated_answer=ask_user + 2
-"#give output"
-print(ask_user + 2)
+#get information
+ask_user = float(input("please give me a number"))
+
+#do calculation
+calculated_answer = ask_user + 2
+
+#give output
+print(calculated_answer)
